f4af19cac804eaf9091dd8276439b7f0458098d0
zorichDev <ffranzor@yahoo.com>
2025-01-24 14:53:24 +0100

    Initial commit: js code update

--- email.js
+++ email.js
@@ -30,3 +30,24 @@
 //             alert("Failed to send email. Please try again later.");
 //         });
 // }
+
+function sendMail() {
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+        subject: document.getElementById("subject").value,
+    };
+
+    emailjs.init("3PD5AyCly9DCyS4u1");
+    
+    emailjs.send("service_ptzp7rd", "template_uvkbxrd", params)
+        .then(function (response) {
+            console.log("Full response:", response);
+            alert("Email sent successfully!");
+        })
+        .catch(function (error) {
+            console.error("Detailed error:", error);
+            alert(`Failed: ${error.text}`);
+        });
+}
